f6a8eb15ded712ca743541810892e94e475a329e
Arnab Mukherjee <arnabmsdn@hotmail.com>
2021-07-03 19:04:09 +0530

    WebRTC-Capture Video

---
+++ Sample-Code/Sample-Code/Controllers/VideoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sample_Code.Controllers
+{    
+    public class VideoController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveRecoredFile()
+        {
+            if( Request.Form.Files.Any())
+            {             
+                var file = Request.Form.Files["video-blob"];
+                string UploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UploadedFiles");
+                string UniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName+ ".webm";
+                string UploadPath = Path.Combine(UploadFolder, UniqueFileName);
+                await file.CopyToAsync(new FileStream(UploadPath, FileMode.Create));
+            }           
+            return Json(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile()
+        {
+            var fileUrl = AppDomain.CurrentDomain.BaseDirectory + "UploadedFiles/" + Request.Form["delete-file"] + ".webm";
+            new FileInfo(fileUrl).Delete();
+            return Json(true);
+        }
+    }
+}
---
+++ Sample-Code/Sample-Code/wwwroot/js/main.js
@@ -0,0 +1,200 @@
+﻿/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+// This code is adapted from
+// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
+
+'use strict';
+
+/* globals MediaRecorder */
+
+let mediaRecorder;
+let recordedBlobs;
+
+const codecPreferences = document.querySelector('#codecPreferences');
+
+const errorMsgElement = document.querySelector('span#errorMsg');
+const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+    if (recordButton.textContent === 'Start Recording') {
+        startRecording();
+    } else {
+        stopRecording();
+        recordButton.textContent = 'Start Recording';
+        playButton.disabled = false;
+        downloadButton.disabled = false;
+        codecPreferences.disabled = false;
+    }
+});
+
+const playButton = document.querySelector('button#play');
+playButton.addEventListener('click', () => {
+    const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
+    const superBuffer = new Blob(recordedBlobs, { type: mimeType });
+    recordedVideo.src = null;
+    recordedVideo.srcObject = null;
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    recordedVideo.controls = true;
+    recordedVideo.play();
+});
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+    const blob = new Blob(recordedBlobs, { type: 'video/webm' });
+    const url = window.URL.createObjectURL(blob);   
+    const a = document.createElement('a');    
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.webm';
+    document.body.appendChild(a);
+    a.click();
+    PostBlob(blob);
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 100);
+    
+});
+
+function handleDataAvailable(event) {
+    console.log('handleDataAvailable', event);
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+function getSupportedMimeTypes() {
+    const possibleTypes = [
+        'video/webm;codecs=vp9,opus',
+        'video/webm;codecs=vp8,opus',
+        'video/webm;codecs=h264,opus',
+        'video/mp4;codecs=h264,aac',
+    ];
+    return possibleTypes.filter(mimeType => {
+        return MediaRecorder.isTypeSupported(mimeType);
+    });
+}
+
+function startRecording() {
+    recordedBlobs = [];
+    const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
+    const options = { mimeType };
+
+    try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    } catch (e) {
+        console.error('Exception while creating MediaRecorder:', e);
+        errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+        return;
+    }
+
+    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    recordButton.textContent = 'Stop Recording';
+    playButton.disabled = true;
+    downloadButton.disabled = true;
+    codecPreferences.disabled = true;
+    mediaRecorder.onstop = (event) => {
+        console.log('Recorder stopped: ', event);
+        console.log('Recorded Blobs: ', recordedBlobs);
+    };
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+    console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+}
+
+function handleSuccess(stream) {
+    recordButton.disabled = false;
+    console.log('getUserMedia() got stream:', stream);
+    window.stream = stream;
+
+    const gumVideo = document.querySelector('video#gum');
+    gumVideo.srcObject = stream;
+
+    getSupportedMimeTypes().forEach(mimeType => {
+        const option = document.createElement('option');
+        option.value = mimeType;
+        option.innerText = option.value;
+        codecPreferences.appendChild(option);
+    });
+    codecPreferences.disabled = false;
+}
+
+function PostBlob(blob) {
+    // FormData
+    var formData = new FormData();  
+    formData.append('video-blob', blob);
+
+    // progress-bar
+    //var hr = document.createElement('hr');
+    //container.appendChild(hr);
+    //var strong = document.createElement('strong');
+    //strong.id = 'percentage';
+    //strong.innerHTML = 'Video upload progress: ';
+    //container.appendChild(strong);
+    //var progress = document.createElement('progress');
+    //container.appendChild(progress);
+   
+    // POST the Blob  
+    $.ajax({
+        type: 'POST',
+        url: "Video/SaveRecoredFile",
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (result) {
+            if (result) {
+                console.log('Success');
+            }
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    })
+}
+
+async function init(constraints) {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    } catch (e) {
+        console.error('navigator.getUserMedia error:', e);
+        errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+    }
+}
+
+document.querySelector('button#start').addEventListener('click', async () => {
+    document.querySelector('button#start').disabled = true;
+    document.querySelector('button#stop').disabled = false;
+    const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
+    const constraints = {
+        audio: {
+            echoCancellation: { exact: hasEchoCancellation }
+        },
+        video: {
+            width: 1280, height: 720
+        }
+    };
+    console.log('Using media constraints:', constraints);
+    await init(constraints);
+});
+
+
+document.querySelector('button#stop').addEventListener('click',async () => {
+    document.querySelector('button#stop').disabled = true;
+    document.querySelector('button#start').disabled = false;
+    const video = document.querySelector('video#gum');    
+    const mediaStream = video.srcObject;   
+    await mediaStream.getTracks().forEach(track => track.stop());
+    video.srcObject = null;
+});
